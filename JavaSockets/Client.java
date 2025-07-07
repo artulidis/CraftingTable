994603eb22ab087049a76aefc0cfa6cf583aa202
akakul2 <akakul2@wgu.edu>
2025-07-07 14:47:55 -0400

    JavaSockets: Client outline ready

--- JavaSockets/Client.java
+++ JavaSockets/Client.java
@@ -1,7 +1,9 @@
 import java.net.Socket;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 /* 
 Client:
@@ -26,3 +28,59 @@ Server:
 - Close the server when done
 - Catch SocketTimeoutException and IOException
  */
+
+ class Client {
+    public static void main(String args[]) {
+        Socket client = null;
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+
+        try {
+            client = new Socket(host, port);
+
+            DataInputStream dataInputStream = new DataInputStream(client.getInputStream());
+            DataOutputStream dataOutputStream = new DataOutputStream(client.getOutputStream());
+
+            Scanner userInput = new Scanner(System.in);
+
+            System.out.println("Server says: " + dataInputStream.readUTF());
+
+            int operation, operand1, operand2;
+            
+            do {
+                System.out.print("Enter operation: ");
+                operation = userInput.nextInt();
+                System.out.print("Enter first operand: ");
+                operand1 = userInput.nextInt();
+                System.out.print("Enter second operand: ");
+                operand2 = userInput.nextInt();
+
+                dataOutputStream.writeInt(operation);
+                dataOutputStream.writeInt(operand1);
+                dataOutputStream.writeInt(operand2);
+
+                System.out.println("Result: " + dataInputStream.readInt());
+            }
+
+            while(operation != 0);
+
+            userInput.close();
+
+        }
+
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+        finally {
+            try {
+                client.close();
+            }
+
+            catch(Exception e) {
+                System.out.println("Couldn't close socket with exception \": " + e + "\"");
+            }
+        }
+
+    }
+}
