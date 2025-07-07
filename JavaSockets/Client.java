608403b75f1c72a77a9c70414e4d532b3928bebc
akakul2 <akakul2@wgu.edu>
2025-07-07 16:15:15 -0400

    JavaSockets: Wrote outline for server

--- JavaSockets/Client.java
+++ JavaSockets/Client.java
@@ -15,18 +15,6 @@ Client:
 - Print server result
 - Look for IOException, NoSuchElementException
 - Finally, try to close the connection
-
-Server:
-- Inherits Thread class and overrides run method
-- Define ServerSocket, and construct the Server with server socket and timeout (throws IOException)
-- Main code captures port on which server listens on and tries to create the Server instance (listens for IOExceptions)
-- Run method indefinitely tries to accept a connection from a client and return a server socket instance
-- Server initializes its own input and output data streams
-- Server sends initialization message to client and receives math operation and operands from client
-- Based on math operation, calculate and write result to client. Break if operation is zero
-- Listen for next inputs
-- Close the server when done
-- Catch SocketTimeoutException and IOException
  */
 
  class Client {
@@ -71,7 +59,7 @@ Server:
         catch(IOException e) {
             e.printStackTrace();
         }
-        
+
         finally {
             try {
                 client.close();
