911b4142619dfb1b8332d9411873f5162f651464
artulidis <artulidis@gmail.com>
2025-07-09 14:19:05 -0400

    JavaSockets: Fixed minor issue

--- JavaSockets.java
+++
@@ -1,9 +0,0 @@
-public class JavaSockets {
-    public static void main(String args[]) {
-        String host = args[0];
-        String port = args[1];
-
-        Server.main(new String[] {port});
-        Client.main(new String[] {host, port});
-    }
-}
