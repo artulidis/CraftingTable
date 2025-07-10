8217af2d472b024bf2848987f367a338b051c319
artulidis <artulidis@gmail.com>
2025-07-10 19:46:44 -0400

    TCPWebServer: MVP ready

--- TCPWebServer/src/Server.java
+++ TCPWebServer/src/Server.java
@@ -1,10 +1,29 @@
 package TCPWebServer.src;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /*
  Stateless application class that creates a ServerSocket on specified port, and indefinitely accepts new clients
  After a successful client connection, a Thread initialized with a Connection, passed with a ServerSocket, is created and started
  */
 
 public class Server {
-    
+    public static void main(String args[]) {
+        int port = Integer.parseInt(args[0]);
+        
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            serverSocket.setSoTimeout(100000);
+
+            while(true) {
+                Socket connectionSocket = serverSocket.accept();
+                Thread connection = new Thread(new Connection(connectionSocket));
+                connection.start();
+            }
+        }
+        catch(IOException e) { e.printStackTrace(); }
+
+    }
 }
